7a07156ac4c496f952a65df2771a144ca02aa453
JonDW99 <116831469+JonDW99@users.noreply.github.com>
2023-04-29 14:48:56 -0700

    Added variables for the servos.

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <kipr/wombat.h>
 // Left motor port - 3
 // Right motor port - 0
+int clawArm = 0;
+int clawHand = 2;
+
 void driveForward(float x, float y){
     y = y * 1000;
     motor(0, x);
